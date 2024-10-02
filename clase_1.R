838847d1ab9ab4b4afeff0b3a6b553aef701b89f
unknown <leyodesa@hotmail.com>
2024-10-02 17:00:56 -0500

    mi primer repo

---
+++ clase_1.R
@@ -0,0 +1,21 @@
+
+x<-"Hola"
+y <- "UNTRM"
+
+
+paste(x,y, sep="-")
+areacirc=function(radio)
+  {
+  pi*radio*radio
+}
+areacirc(10)
+
+IMC=function(peso,talla){ 
+  peso/(talla)^2
+  
+}
+
+IMC(76,1.65)
+
+  
+
